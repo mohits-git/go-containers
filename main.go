7195b49f2d68fbde3fd1b68c93aac9e736b6c8c1
mohits-git <mohitkhatri2805@gmail.com>
2024-10-07 18:47:36 +0530

    init: basic code for running commands, setting namespaces for linux, no-workaround for macOS

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"syscall"
+)
+
+/// What we are building?
+// docker         run image <cmd> <params>
+// building equivalent:
+// go run main.go run       <cmd> <params>
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+
+	default:
+		panic("bad comment")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if runtime.GOOS == "linux" {
+        fmt.Println("Warning: If you're on Linux, uncomment the code manually")
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			// Cloneflags: syscall.CLONE_NEWUTS,
+		}
+	} else if runtime.GOOS == "darwin" {
+		fmt.Println("Warning: macOS does not support Linux namespaces")
+	}
+
+    must(cmd.Run())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
