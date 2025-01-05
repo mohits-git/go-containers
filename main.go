8851c884023e2e82390059bc9f2259163da28330
mohits-git <152606488+mohits-git@users.noreply.github.com>
2025-01-05 19:22:09 +0530

    add cgroup for our container

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
+	"strconv"
 	"syscall"
 )
 
@@ -21,17 +23,17 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-  case "child":
-    child()
+	case "child":
+		child()
 	default:
 		panic("bad comment")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:], "as", os.Getpid())
+	fmt.Printf("Running %v as %v\n", os.Args[2:], os.Getpid())
 
-  cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -43,25 +45,27 @@ func run() {
 	}
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-    Unshareflags: syscall.CLONE_NEWNS,
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
 
 	must(cmd.Run())
 }
 
 func child() {
-	fmt.Printf("Running %v\n", os.Args[2:], "as", os.Getpid())
+	fmt.Printf("Running %v as %v\n", os.Args[2:], os.Getpid())
 
-  syscall.Sethostname([]byte("mycontainer"))
-  // pwd
-  path, err := os.Getwd()
-  if err != nil {
-    panic(err)
-  }
-  syscall.Chroot(path + "/rootfs")
-  syscall.Chdir("/")
-  syscall.Mount("proc", "proc", "proc", 0, "")
+  cg()
+
+	syscall.Sethostname([]byte("mycontainer"))
+	// pwd
+	path, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	syscall.Chroot(path + "/rootfs")
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 
@@ -71,7 +75,18 @@ func child() {
 
 	must(cmd.Run())
 
-  syscall.Unmount("/proc", 0)
+	syscall.Unmount("/proc", 0)
+}
+
+func cg() {
+	pids := "/sys/fs/cgroup/pids"
+	err := os.Mkdir(filepath.Join(pids, "liz"), 0755)
+	if err != nil && !os.IsExist(err) {
+		panic(err)
+	}
+	must(os.WriteFile(filepath.Join(pids, "liz/pids.max"), []byte("20"), 0700))
+	must(os.WriteFile(filepath.Join(pids, "liz/notify_on_release"), []byte("1"), 0700))
+	must(os.WriteFile(filepath.Join(pids, "liz/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
 
 func must(err error) {
