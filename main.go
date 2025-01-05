78460a5696d94346df939bf04a7da63611846d10
mohits-git <152606488+mohits-git@users.noreply.github.com>
2025-01-05 18:31:02 +0530

    feat: create new container with separate namespaces and root (ubuntu rootfs)

--- main.go
+++ main.go
@@ -21,14 +21,47 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-
+  case "child":
+    child()
 	default:
 		panic("bad comment")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
+	fmt.Printf("Running %v\n", os.Args[2:], "as", os.Getpid())
+
+  cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if runtime.GOOS != "linux" {
+		fmt.Println("this operating system is not supported")
+		os.Exit(0)
+	}
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+    Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2:], "as", os.Getpid())
+
+  syscall.Sethostname([]byte("mycontainer"))
+  // pwd
+  path, err := os.Getwd()
+  if err != nil {
+    panic(err)
+  }
+  syscall.Chroot(path + "/rootfs")
+  syscall.Chdir("/")
+  syscall.Mount("proc", "proc", "proc", 0, "")
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 
@@ -36,16 +69,9 @@ func run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-  if runtime.GOOS != "linux" {
-    fmt.Println("this operating system is not supported")
-    os.Exit(0)
-  }
-
- 	cmd.SysProcAttr = &syscall.SysProcAttr{
- 		 Cloneflags: syscall.CLONE_NEWUTS,
- 	}
-
 	must(cmd.Run())
+
+  syscall.Unmount("/proc", 0)
 }
 
 func must(err error) {
